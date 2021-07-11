77f354b2d52f4a90b7934e6cbcec2ac5e5a796df
Kaiming Cui <ckm@nao.cas.cn>
2021-07-11 17:51:03 +0800

    fix: fix action bug caused by omitted light curves

--- tests/test_deep_transit_detection.py
+++ tests/test_deep_transit_detection.py
@@ -16,10 +16,14 @@ def test_detection():
     import os
     lc = kepler_id_to_lc(11446443).stitch()
     lc.sort('time')
-
     lc = lc[lc.time.value < 150]
+
+    print(len(lc))
+
+
     dt_obj = dt.DeepTransit(lc, is_flatten=False, flatten_kwargs={'window_length': 0.5, 'sigma_upper': 3})
-    os.system("wget http://paperdata.china-vo.org/ckm/kepler_snr_model.pth -P models/")
+    if not os.path.exists('models/kepler_snr_model.pth'):
+        os.system("wget http://paperdata.china-vo.org/ckm/kepler_snr_model.pth -P models/")
     bboxes = dt_obj.transit_detection('models/kepler_snr_model.pth', batch_size=3)
 
     assert len(bboxes) > 0
